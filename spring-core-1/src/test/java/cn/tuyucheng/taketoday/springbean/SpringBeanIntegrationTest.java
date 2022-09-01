863f38c6ddea1ebd1b8ac2d1deb61dce8e50fde1
tuyucheng <tuyucheng2000@163.com>
2022-09-01 21:44:10 +0800

    What is a Spring Bean?

---
+++ spring-core-1/src/test/java/cn/tuyucheng/taketoday/springbean/SpringBeanIntegrationTest.java
@@ -0,0 +1,21 @@
+package cn.tuyucheng.taketoday.springbean;
+
+import cn.tuyucheng.taketoday.springbean.domain.Company;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SpringBeanIntegrationTest {
+    
+    @Test
+    @DisplayName("whenUsingIoC_thenDependenciesAreInjected")
+    void whenUsingIoC_thenDependenciesAreInjected() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Company company = context.getBean("company", Company.class);
+        assertEquals("High Street", company.getAddress().getStreet());
+        assertEquals(1000, company.getAddress().getNumber());
+    }
+}
