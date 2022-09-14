61b89c9af3a325fefca3442592b165097388bd8c
tuyucheng <tuyucheng2000@163.com>
2022-09-14 18:19:16 +0800

    replace standard output by a logger

--- java-concurrency-simple/src/main/java/cn/tuyucheng/taketoday/concurrent/daemon/NewThread.java
+++ java-concurrency-simple/src/main/java/cn/tuyucheng/taketoday/concurrent/daemon/NewThread.java
@@ -11,7 +11,6 @@ public class NewThread extends Thread {
         while (true) {
             for (int i = 0; i < 10; i++) {
                 log.info("{}: New Thread is running... {}", this.getName(), i);
-                System.out.println(this.getName() + ": New Thread is running..." + i);
                 try {
                     // Wait for one sec, so it doesn't print too fast
                     Thread.sleep(1000);
