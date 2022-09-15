c24f3febe8822e3c9a1c7f102a18b35ecabf9831
tuyucheng <tuyucheng2000@163.com>
2022-09-15 08:31:36 +0800

    Spring @Primary Annotation

---
+++ spring-framework-core-2/src/main/java/cn/tuyucheng/taketoday/primary/Employee.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.primary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private final String name;
+}
